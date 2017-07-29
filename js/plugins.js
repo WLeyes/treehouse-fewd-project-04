c84a1b7f510a3514b75163e53b8924b7b78228a6
WLeyes <warren@leyes.ca>
2017-07-28 22:09:46 -0700

    test swipe plugin

--- js/plugins.js
+++ js/plugins.js
@@ -23,4 +23,14 @@
 
 // Place any jQuery/helper plugins in here.
 
+$(function() {
+    $("#test").swipe( {
+        //Generic swipe handler for all directions
+        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+            $(this).text("You swiped " + direction );
+        }
+    });
 
+    //Set some options later
+    $("#test").swipe( {fingers:2} );
+});

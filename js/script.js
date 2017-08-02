2031075d78cac7f972c6245bb35ef67abc2657ae
WLeyes <warren@leyes.ca>
2017-08-01 17:16:42 -0700

    added Fontawesome light fonts, started to rebuild overlay.js file, tweeked some css for dev mode

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
+// this is set get and set local storage to prevent the intro from playing everytime the lightbox gets closed
+// can remove the local storage cookie in dev console ;)
+
+function intro(){
+    var ls = window.localStorage;
+    if(ls.getItem('intro') === 'undefined' || ls.getItem('intro') === null)
+    {
+        ls.setItem('intro', true);
+    } // I think i need to add a for loop here to loop through all objects and append class name
+    if(ls.getItem('intro') === 'true'){
+        document.getElementsByClassName('imageItem').className += 'intro';
+        document.getElementsByClassName('gallery').className += 'intro2';
+    } else if(ls.getItem('intro') === 'false'){
+        document.getElementsByClassName('imageItem').remove('intro');
+        document.getElementsByClassName('gallery').remove('intro2');
+    } else {
+        // future usage
+    }
+}
+intro();
 /**
  * Created by Warren Leyes on 2017-07-25.
  */
@@ -10,7 +30,6 @@ var images, input, imageItem, i;
 
     input.onkeyup = function () {
         for(i=0; i < images.length; i++){
-            console.log(input.value.toUpperCase());
             if (images[i].getAttribute('title').toUpperCase().indexOf(input.value.toUpperCase()) <= -1) {
                 //hide images that don't contain keyword from input
                 document.getElementsByClassName('imageItem')[i].style.display = 'none';
@@ -18,19 +37,17 @@ var images, input, imageItem, i;
                 document.getElementsByClassName('imageItem')[i].style.display = 'block';
                 window.localStorage.setItem('intro', 'true');
             }
+            // Clear input after the image has been clicked
             var result = document.getElementsByClassName('imageItem')[i];
-            console.log(result);
-            if(document.onblur = function () {
-                    console.log('blur');
-                })
-                // .onblur clear the input and reset the gallery
                 input.onblur = function () {
-                    this.value = '';
-                    for(i=0; i < images.length; i++){
-                        document.querySelectorAll('.imageItem')[i].style.display = 'block';
-                    }
-                };
-        }
+                    $('.image').on('click', function(event) {
+                        this.value = '';
+                        for (i = 0; i < images.length; i++) {
+                            document.querySelectorAll('.imageItem')[i].style.display = 'block';
+                        } // end for()
+                    })// end click
+                }; // end onblur()
+        }// end for
     };
 }// EOF
 searchAndDetroy();
@@ -59,42 +76,47 @@ $(document).ready(function() {
  */
 
 $(document).ready(function() {
-    //Global variables
-    var gallery, image, imageItem, background, headerText, captionText;
-    gallery =       document.querySelector('.gallery');
-    imageItem =     document.getElementsByClassName('imageItem');
-    image =         document.getElementsByClassName('image');
-    headerText =    document.getElementsByClassName('imageTitle');
-    captionText =   document.getElementsByClassName('imageText');
-    $src =          null;
-    $image =   0;
-    lightbox = {
-        background: {id:    'background', class: 'overlayBackground', src:   $src},
-        imageTitle:  headerText,
-        imageCaption: captionText
-    };
-    for(i = 0; i < imageItem.length; i++){
-         lightbox.background.src    = largeImagePath(image[i].src);
-         lightbox.imageTitle = headerText[i].innerHTML;
-         lightbox.imageCaption = captionText[i].innerHTML;
-        console.log(lightbox.imageTitle);
-        console.log(lightbox.background);
-        console.log(lightbox.imageCaption);
-    }
+    // Declare variables
+    let gallery,imageItem, image,imagePath,newImagePath, $src;
 
     // determine what was clicked at gallery & children level
-    gallery.addEventListener('click', function(event) {
+    imageItem = document.getElementsByClassName('image');
+    for(let i =0; i < imageItem.length; i++){
+        imageItem[i].addEventListener('click', (event) =>{
+            lightbox = {
+                galleryTag: '<article id="background" class="overlayBackground"></article>',
+                background: largeImagePath(event.target.src),
+                elements: {
+                    closeButton:   '<i id="closeButton"    class="fal fa-times-circle"       aria-hidden="true"></i>',
+                    nextButton:    '<i id="nextButton"     class="fal fa-angle-double-right" aria-hidden="true"></i>',
+                    previousButton:'<i id="previousButton" class="fal fa-angle-double-left"  aria-hidden="true"></i>'
+                },// end elements
+                image: {
+                    imageTag:  '<section id="largeImage" class="overlay"></section>',
+                    src: event.target.src,
+                    alt: event.target.alt,
+                    title: event.target.title,
+                    headerText: event.target.previousElementSibling.innerHTML,
+                    captionText: event.target.nextElementSibling.innerHTML,
+                    index: i
+                }// end image
+            };// end lightbox
+            console.log('Index: '               + lightbox.image.index);
+            console.log('Lightbox Background: ' + lightbox.background);
+            console.log('Lighbox Header: '      + lightbox.image.headerText);
+            console.log('Lighbox Image: '       + lightbox.image.src);
+            console.log('Lightbox Caption: '    + lightbox.image.captionText);
 
-    });// end .click()
+            //test: goal try to use event target ++ -- to get next and previous images, maybe setting an index
 
 
-
+        });// end click()
+    }// end for()
 
     // string replace image path
     function largeImagePath(path) {
-        var imagePath = 'img/thumbnails/';
-        var newImagePath = 'img/';
+        imagePath = 'img/thumbnails/';
+        newImagePath = 'img/';
         return path.replace(imagePath, newImagePath);
-    }
-
+    }// end largeImage()
 }); // EOF
